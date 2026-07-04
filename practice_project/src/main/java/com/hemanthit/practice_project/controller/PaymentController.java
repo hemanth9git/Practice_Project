74efed31256dad8391f0fdff2ac862577baad3ca
hemanth9git <hemanthsaikondaveeti99@gmail.com>
2026-07-04 14:11:17 +0530

    Implemented the loggers in the payment class

--- practice_project/src/main/java/com/hemanthit/practice_project/controller/PaymentController.java
+++ practice_project/src/main/java/com/hemanthit/practice_project/controller/PaymentController.java
@@ -1,9 +1,14 @@
 package com.hemanthit.practice_project.controller;
 
+import com.hemanthit.practice_project.dto.PaymentRequest;
 import com.hemanthit.practice_project.model.Payment;
 import com.hemanthit.practice_project.serviceImpl.PaymentService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("payment")
 public class PaymentController {
 
+    private static final Logger logger =
+            LoggerFactory.getLogger(PaymentController.class);
+
     PaymentService service;
 
-   
+    @PostMapping("savePayment")
+   public ResponseEntity<Payment> savePayment(@RequestBody PaymentRequest request){
+        logger.info("Started the payment...");
+        logger.info("Sending the req to payment service");
+        Payment payment=service.savePayment(request);
+        return new ResponseEntity<>(payment, HttpStatus.CREATED);
+   }
 
 }
--- practice_project/src/main/java/com/hemanthit/practice_project/serviceImpl/PaymentService.java
+++ practice_project/src/main/java/com/hemanthit/practice_project/serviceImpl/PaymentService.java
@@ -1,13 +1,47 @@
 package com.hemanthit.practice_project.serviceImpl;
 
+import com.hemanthit.practice_project.controller.PaymentController;
+import com.hemanthit.practice_project.dto.PaymentRequest;
+import com.hemanthit.practice_project.model.Payment;
 import com.hemanthit.practice_project.repository.PaymentRepository;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
 @AllArgsConstructor
 @Service
 public class PaymentService {
 
+    private static final Logger logger =
+            LoggerFactory.getLogger(PaymentController.class);
     PaymentRepository repository;
 
+    public Payment savePayment(PaymentRequest request) {
 
+        Payment payment=new Payment();
+        payment.setAmount(request.getAmount());
+        payment.setPayerName(request.getPayerName());
+        int balance=10000;
+        logger.info("Received the payment request");
+        if(request.getAmount()>100000){
+            logger.warn("Fraud alert..");
+        }
+        if(request.getAmount()>balance){
+            logger.error("Transaction amount greater than balance and payment failed..");
+            payment.setTransactionId(UUID.randomUUID().toString());
+            payment.setStatus("Payment failed..");
+            return repository.save(payment);
+        }else{
+            logger.info("Payment in progress..");
+            payment.setTransactionId(UUID.randomUUID().toString());
+            payment.setStatus("Payment Success..");
+            logger.info("Payment successs...");
+            return repository.save(payment);
+        }
+
+
+    }
 }
